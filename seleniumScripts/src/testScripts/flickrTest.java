f0c4761c2aa66c0b4a931a96cfac1351469dbb1f
Anik <anik@agiletech.com>
2020-03-08 13:38:12 -0400

    updated

---
+++ seleniumScripts/src/testScripts/flickrTest.java
@@ -0,0 +1,30 @@
+package testScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class flickrTest {
+
+	public static void main(String[] args) {
+		
+		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\Agile1Tech\\Desktop\\programming libraries\\chromedriver.exe");
+			
+			WebDriver driver = new ChromeDriver();
+			
+			driver.manage().deleteAllCookies();
+			
+			// AUT to navigate 
+			
+			driver.navigate().to("https://identity.flickr.com/sign-up");
+			
+			
+			WebElement firstname = driver.findElement(By.id("sign-up-first-name"));
+			WebElement lastname = driver.findElement(By.id("sign-up-last-name"));
+		
+           firstname.sendKeys("Michael");
+           lastname.sendKeys("Jordan");
+	}
+
+}
